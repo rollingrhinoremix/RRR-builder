f1ed40cf1cc0bab9b01f471a7cc2ee1e12994487
cat-master21 <96554164+cat-master21@users.noreply.github.com>
2022-06-02 17:04:40 -0700

    Update sign.sh

--- .github/workflows/sign.sh
+++ .github/workflows/sign.sh
@@ -3,6 +3,7 @@
 echo "$KEY" | sudo tee ~/RRR-builder/private.pgp > /dev/null
 GPG_TTY=$(tty)
 export GPG_TTY
-echo "$PASSPHRASE" | sudo gpg --batch --allow-secret-key-import --import ~/RRR-builder/private.pgp
+sudo echo "$PASSPHRASE" | sudo gpg --batch --allow-secret-key-import --import ~/RRR-builder/private.pgp
 source ~/RRR-builder/build.conf
-gpg -o ~/RRR-builder/"$OUT_ISO".sig -u A2C555536C8418110643D071945D30DAB6C43922 --batch -ab ~/RRR-builder/"$OUT_ISO"
+echo "$PASSPHRASE" | sudo gpg -o ~/RRR-builder/"$OUT_ISO".sig --with-colons --status-fd 0 --command-fd 0 --no-batch --pinentry-mode loopback -u 945D30DAB6C43922 -ab ~/RRR-builder/"$OUT_ISO"
+exit 0
