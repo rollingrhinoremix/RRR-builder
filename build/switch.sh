437dd2bfd3d13ad2d50ef3027c4e1601bef3680b
Chadano <airriflepellet@gmail.com>
2022-10-09 14:44:44 +0100

    fix: no installer
    So far I am testing this by modifying the minimal installation of RRR, this NEEDS to be the standard installation by 2023

--- build/switch.sh
+++ build/switch.sh
@@ -4,8 +4,6 @@
 apt-get update -y
 # Initialise the development by grabbing assets
 apt-get install git xdg-user-dirs -yq # These are mostly already installed. Just setting it to manually installed for minimal
-# Remove GNOME
-apt-get remove --purge gnome* ubuntu-session ubuntu-desktop-minimal -y
 # Perform system upgrade
 apt-get update -y
 apt-get upgrade -y
@@ -28,7 +26,7 @@ apt-get clean -y
 sed -i 's/kinetic/devel/g' /etc/apt/sources.list
 sed -i 's/kinetic/devel/g' /etc/lsb-release
 sed -i 's/kinetic/devel/g' /usr/lib/os-release
-#So much release info that are mostly same!
+# So much release info that are mostly same!
 sed -i 's/PRETTY_NAME="Ubuntu Kinetic Kudu (development branch)"/PRETTY_NAME="Rolling Rhino Linux"/g' /etc/os-release
 sed -i 's%HOME_URL="https://www.ubuntu.com/"%HOME_URL="https://rollingrhino.org"%g' /etc/os-release
 apt-get --allow-releaseinfo-change update -y
